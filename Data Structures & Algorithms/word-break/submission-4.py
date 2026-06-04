4d9df6b3a927e7f9d8c303eee2c2d5fe42703655
dhruvphansalkar <42715581+dhruvphansalkar@users.noreply.github.com>
2026-06-04 13:16:23 -0500

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.py
@@ -0,0 +1,36 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # in the recurive solution:
+        # if at len of s return true
+        # iterate from i+1 to the remaining string
+        # if substring is in wordDict we have 2 options
+        # take the word and start recursive call
+        # keep going
+        # return or from the result
+
+        dp = [False] * (len(s) + 1)
+        dp[-1] = True
+        words = set(wordDict)
+        seen = dict()
+
+        for i in range(len(s) - 1, -1, -1):
+            canReachEnd = False
+            for j in range(i+1, len(s) + 1):
+                if s[i:j] in words:
+                    canReachEnd = canReachEnd or dp[j]
+            dp[i] = canReachEnd
+        return dp[0]
+
+        def recur(i: int) -> int:
+            if i in seen:
+                return seen[i]
+            if i == len(s):
+                return True
+            canReachEnd = False
+            for j in range(i+1, len(s) + 1):
+                if s[i:j] in words:
+                    canReachEnd = canReachEnd or recur(j)
+            seen[i] = canReachEnd
+            return seen[i]
+        
+        return recur(0)
